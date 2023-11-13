99e5e30118f09b27e45af9634b904043fbb1f388
Luka Macharashvili <macharashvililuka12@gmail.com>
2023-11-14 00:00:30 +0400

    ...

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    // User Table
+    // (unique) | (unique) | (unique) | Date | Date
+    // id | username | email | created_at | updated_at
+
+    // Repository Table | Many to one with User
+    // (unique) | (unique) | string | (unique, AWS S3 public-read bucket url) | Date | Date
+    // id | name | description | url | created_at | updated_at
+
+    // Basic user management (Sync between database and AWS Cognito)
+    // - Register
+    // - Login
+    // - Logout
+    // - Update
+    // - Delete (also delete all repos)
+
+    // Basic repository management
+    // - Create
+    // - Update
+    // - Delete
 }
