c41c2a3ead9702eb1777410863979ac04dffd6a8
Luka Macharashvili <macharashvililuka12@gmail.com>
2023-11-17 00:55:51 +0400

    delete_contents_of_directory concurrent

--- cli/src/utils/fs_provider.rs
+++ cli/src/utils/fs_provider.rs
@@ -5,6 +5,7 @@ use std::io;
 use std::io::{Read, Write};
 use std::io::{Result, Seek, SeekFrom};
 use std::path::{Path, PathBuf};
+use std::thread;
 
 use super::load_ignores;
 
@@ -77,17 +78,27 @@ pub fn delete_contents_of_directory(path: &str, ignores: Option<&Vec<String>>) -
     };
     let files = traverse_directory(Some(Path::new(path)), Some(ignores));
     let files_to_ignore = load_ignores();
+    let mut join_handles = Vec::new();
 
     for entry_path in files {
-        let entry_name = entry_path.file_name().unwrap().to_str().unwrap();
-        if files_to_ignore.contains(&entry_name.to_owned()) {
-            continue;
-        }
-        if entry_path.is_file() {
-            fs::remove_file(entry_path)?;
-        } else if entry_path.is_dir() {
-            fs::remove_dir_all(entry_path)?;
-        }
+        let files_to_ignore = files_to_ignore.clone();
+        let handle = thread::spawn(move || {
+            let entry_name = entry_path.file_name().unwrap().to_str().unwrap();
+            if files_to_ignore.contains(&entry_name.to_owned()) {
+                return;
+            }
+            if entry_path.is_file() {
+                fs::remove_file(entry_path).unwrap();
+            } else if entry_path.is_dir() {
+                fs::remove_dir_all(entry_path).unwrap();
+            }
+        });
+
+        join_handles.push(handle);
+    }
+
+    for handle in join_handles {
+        handle.join().unwrap();
     }
 
     Ok(())
